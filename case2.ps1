ca3d7efe055d11724e6ee6189c94667756ebcb5e
emilh93 <57196515+emilh93@users.noreply.github.com>
2019-11-08 15:16:59 +0100

    Update case2.ps1

--- case2.ps1
+++ case2.ps1
@@ -1,9 +1,5 @@
 netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol="icmpv4:8,any" dir=in action=allow
-
-Test-NetConnection -ComputerName logaccount123.file.core.windows.net -Port 445
-# Save the password so the drive will persist on reboot
-Invoke-Expression -Command "cmdkey /add:logaccount123.file.core.windows.net /user:Azure\logaccount123 /pass:rwAEY/uMx/Zj4ZgNsWNRcl+9TtvZm8+lvHVDL5fRCaBNF5Sh7L1+bE/YuEvx6fsZnL+66exMTWXx53ZPFI8OHQ=="
-# Mount the drive
-New-PSDrive -Name Z -PSProvider FileSystem -Root "\\logaccount123.file.core.windows.net\logshared"
-
+cmdkey /add:logaccount123.file.core.windows.net /user:Azure\logaccount123 /pass:rwAEY/uMx/Zj4ZgNsWNRcl+9TtvZm8+lvHVDL5fRCaBNF5Sh7L1+bE/YuEvx6fsZnL+66exMTWXx53ZPFI8OHQ==
+net use Z: \\logaccount123.file.core.windows.net\logshared /persistent:Yes
 Ping 10.0.0.5 > Z:\log.txt
+.
