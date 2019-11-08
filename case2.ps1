19da3d52514ae3b85963cd4d2caccebccac845a6
emilh93 <57196515+emilh93@users.noreply.github.com>
2019-11-08 14:53:05 +0100

    Update case2.ps1

--- case2.ps1
+++ case2.ps1
@@ -1,14 +1,9 @@
 netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol="icmpv4:8,any" dir=in action=allow
 
-ping 10.0.0.5 > C:\Users\Student\Desktop\log1.txt
+Test-NetConnection -ComputerName logaccount123.file.core.windows.net -Port 445
+# Save the password so the drive will persist on reboot
+Invoke-Expression -Command "cmdkey /add:logaccount123.file.core.windows.net /user:Azure\logaccount123 /pass:rwAEY/uMx/Zj4ZgNsWNRcl+9TtvZm8+lvHVDL5fRCaBNF5Sh7L1+bE/YuEvx6fsZnL+66exMTWXx53ZPFI8OHQ=="
+# Mount the drive
+New-PSDrive -Name Z -PSProvider FileSystem -Root "\\logaccount123.file.core.windows.net\logshared"
 
-
-$storageAccount = Get-AzStorageAccount -ResourceGroupName Test2 `
-  -Name "eheu01129323" `
-
-$ctx = $storageAccount.Context 
-
-Set-AzStorageBlobContent -File "C:\Users\Students\Desktop\log1.txt" `
-  -Container "blob1" `
-  -Blob "log1.txt" `
-  -Context $ctx
+Ping 10.0.0.5 > Z:\log.txt
