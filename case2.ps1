2a3f6cb7a2f4fdcfcce375434470b89a63e5cb16
emilh93 <57196515+emilh93@users.noreply.github.com>
2019-11-08 13:34:41 +0100

    Update case2.ps1

--- case2.ps1
+++ case2.ps1
@@ -1,6 +1,6 @@
 netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol="icmpv4:8,any" dir=in action=allow
 
-ping 10.0.0.5 > C:\Users\Students\Desktop\log1.txt
+ping 10.0.0.5 > C:\Users\Student\Desktop\log1.txt
 
 
 $storageAccount = Get-AzStorageAccount -ResourceGroupName Test2 `
