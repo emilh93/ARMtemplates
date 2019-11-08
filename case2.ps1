658a32b89f513f02a5286762beddcd5814f0477f
emilh93 <57196515+emilh93@users.noreply.github.com>
2019-11-08 13:28:45 +0100

    Add files via upload

---
+++ case2.ps1
@@ -0,0 +1,14 @@
+netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol="icmpv4:8,any" dir=in action=allow
+
+ping 10.0.0.5
+
+
+$storageAccount = Get-AzStorageAccount -ResourceGroupName Test2 `
+  -Name "eheu01129323" `
+
+$ctx = $storageAccount.Context 
+
+Set-AzStorageBlobContent -File "C:\Users\Students\Desktop\log1.txt" `
+  -Container "blob1" `
+  -Blob "log1.txt" `
+  -Context $ctx 
