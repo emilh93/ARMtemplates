0a865521ebf6ccb3072cf0692d38de0c54929071
emilh93 <57196515+emilh93@users.noreply.github.com>
2019-11-08 13:31:25 +0100

    Update case2.ps1

--- case2.ps1
+++ case2.ps1
@@ -1,6 +1,6 @@
 netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol="icmpv4:8,any" dir=in action=allow
 
-ping 10.0.0.5
+ping 10.0.0.5 > C:\Users\Students\Desktop\log1.txt
 
 
 $storageAccount = Get-AzStorageAccount -ResourceGroupName Test2 `
@@ -11,4 +11,4 @@ $ctx = $storageAccount.Context
 Set-AzStorageBlobContent -File "C:\Users\Students\Desktop\log1.txt" `
   -Container "blob1" `
   -Blob "log1.txt" `
-  -Context $ctx 
+  -Context $ctx
