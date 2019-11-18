f5e5888d64b480204a03bb3b7fe8abb228f33404
emilh93 <57196515+emilh93@users.noreply.github.com>
2019-11-18 14:26:31 +0100

    Update case2.ps1

--- case2.ps1
+++ case2.ps1
@@ -1,5 +1,1 @@
-netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol="icmpv4:8,any" dir=in action=allow
-cmdkey /add:logaccount123.file.core.windows.net /user:Azure\logaccount123 /pass:rwAEY/uMx/Zj4ZgNsWNRcl+9TtvZm8+lvHVDL5fRCaBNF5Sh7L1+bE/YuEvx6fsZnL+66exMTWXx53ZPFI8OHQ==
-net use Z: \\logaccount123.file.core.windows.net\logshared /persistent:Yes
-Ping 10.0.0.5 > Z:\log.txt
-.
+
